c5c37dd1a772a9b3f47ddfe251acac8dc47a942a
Ryan <kuhnrl30@gmail.com>
2015-10-04 23:59:24 -0400

    Cleanup

--- R/Northwind.R
+++ R/Northwind.R
@@ -1,32 +1,36 @@
-#' Northwind
+#' Northwind: a collection of database tables
 #'
+#'The Northwind package is a collection of database tables from the freely downloadable
+#'Northwind sample database  They are useful datasets for demonstrating such things as
+#'SQL queries and business data analytics.
 #' @name Northwind
 #' @docType package
 NULL
 
 
-#' Northwind data tables
+#' Customer data table
 #'
-#' These data tables are freely downloadable from the Northwind sample database.
+#' This data table is part of the freely downloadable Northwind sample database.
 #' They are useful datasets for demonstrating such things as SQL queries and
-#' business data analytics. Loading this dataset will place the dataframes listed
-#' below nto your environment.  The dataframes are based on a normalized database
-#' and has primary-foreign key relationships in the tables. As an example, the ID
-#' field in the InventoryTransactionType dataframe is a primary key which corresponds
-#' to a foreign key in the InventoryTransactions dataframe.
-#' \itemize{
-#' \item Customers
-#' \item Employees
-#' \item InventoryTransactions
-#' \item InventoryTransactionTYpes
-#' \item Invoices
-#' \item Orders
-#' \item PurchaseOrderDetails
-#' \item Shippers
-#' \item Suppliers
-#' }
-#' @name Northwind
+#' business data analytics. Loading the Northwind dataset will place this and other
+#' related datanto your environment.
+#' @name Customer
 #' @docType data
 #' @usage data(Northwind)
 #' @keywords data
-NULL
+#' @format A dataframe with 29 rows and 12 variables:
+#' \describe{
+#' \item {ID}{primary key}
+#' \item{Compay}{name of the customer}
+#' \item{LastName}{last name of point of contact at the customer}
+#' \item{FirstName}{first name of point of contact at the customer}
+#' \item{JobTitle}{title of point of contact at the customer}
+#' \item{BusinessPhone}{phone number formatted as "(123)456-7890"}
+#' \itme{FaxNumber}{fax number formatted as "(123)456-7890"}
+#' \item{Address}{street number}
+#' \item{City}{City}
+#' \item{StateProvince}{Two digit abbreviation}
+#' \item{ZIPPostalCode}{5 digit zip code}
+#' \item{CountryRegion}{Country}
+#' }
+"Customers"
