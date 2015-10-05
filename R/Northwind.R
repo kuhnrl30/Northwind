f01aff04cceca3a585b335785731fc14852fd8b1
Ryan <kuhnrl30@gmail.com>
2015-10-04 23:11:12 -0400

    initial commit

---
+++ R/Northwind.R
@@ -0,0 +1,32 @@
+#' Northwind
+#'
+#' @name Northwind
+#' @docType package
+NULL
+
+
+#' Northwind data tables
+#'
+#' These data tables are freely downloadable from the Northwind sample database.
+#' They are useful datasets for demonstrating such things as SQL queries and
+#' business data analytics. Loading this dataset will place the dataframes listed
+#' below nto your environment.  The dataframes are based on a normalized database
+#' and has primary-foreign key relationships in the tables. As an example, the ID
+#' field in the InventoryTransactionType dataframe is a primary key which corresponds
+#' to a foreign key in the InventoryTransactions dataframe.
+#' \itemize{
+#' \item Customers
+#' \item Employees
+#' \item InventoryTransactions
+#' \item InventoryTransactionTYpes
+#' \item Invoices
+#' \item Orders
+#' \item PurchaseOrderDetails
+#' \item Shippers
+#' \item Suppliers
+#' }
+#' @name Northwind
+#' @docType data
+#' @usage data(Northwind)
+#' @keywords data
+NULL
---
+++ README.md
@@ -0,0 +1,2 @@
+The purpose of this package is to make the tables in the Northwind database available for demonstrating other techniques in R. 
+The goal is that with other dataset available, we won't have to see so many tutorials using the Iris dataset. :)
