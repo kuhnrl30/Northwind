977908a811dc456946b0e471cb74eaff9b8c3fae
Ryan <kuhnrl30@gmail.com>
2015-10-05 00:19:07 -0400

    Updated data documentation

--- R/Northwind.R
+++ R/Northwind.R
@@ -8,29 +8,75 @@
 NULL
 
 
-#' Customer data table
+#' Northwind Customer data table
 #'
 #' This data table is part of the freely downloadable Northwind sample database.
 #' They are useful datasets for demonstrating such things as SQL queries and
 #' business data analytics. Loading the Northwind dataset will place this and other
 #' related datanto your environment.
-#' @name Customer
-#' @docType data
 #' @usage data(Northwind)
 #' @keywords data
 #' @format A dataframe with 29 rows and 12 variables:
-#' \describe{
-#' \item {ID}{primary key}
-#' \item{Compay}{name of the customer}
-#' \item{LastName}{last name of point of contact at the customer}
-#' \item{FirstName}{first name of point of contact at the customer}
-#' \item{JobTitle}{title of point of contact at the customer}
-#' \item{BusinessPhone}{phone number formatted as "(123)456-7890"}
-#' \itme{FaxNumber}{fax number formatted as "(123)456-7890"}
-#' \item{Address}{street number}
-#' \item{City}{City}
-#' \item{StateProvince}{Two digit abbreviation}
-#' \item{ZIPPostalCode}{5 digit zip code}
-#' \item{CountryRegion}{Country}
+#' \itemize{
+#' \item ID.
+#' \item Company. name of the customer
+#' \item LastName. last name of point of contact at the customer
+#' \item FirstName. first name of point of contact at the customer
+#' \item JobTitle. title of point of contact at the customer
+#' \item BusinessPhone. formatted as "(123)456-7890"
+#' \item FaxNumber. formatted as "(123)456-7890"
+#' \item Address.
+#' \item City.
+#' \item StateProvince. Two digit abbreviation
+#' \item ZIPPostalCode.
+#' \item CountryRegion.
 #' }
 "Customers"
+
+#' Northwind Employees data table
+#'
+#' This data table is part of the freely downloadable Northwind sample database.
+#' They are useful datasets for demonstrating such things as SQL queries and
+#' business data analytics. Loading the Northwind dataset will place this and other
+#' related datanto your environment.
+#' @usage data(Northwind)
+#' @keywords data
+#' @format A dataframe with 9 rows and 15 variables:
+#' \itemize{
+#' \item ID.
+#' \item Company. name of the customer
+#' \item LastName.
+#' \item FirstName.
+#' \item EmailAddress
+#' \item JobTitle.
+#' \item BusinessPhone. formatted as "(123)456-7890"
+#' \item HomePhone. formatted as "(123)456-7890"
+#' \item FaxNumber. formatted as "(123)456-7890"
+#' \item Address.
+#' \item City.
+#' \item StateProvince. Two digit abbreviation
+#' \item ZIPPostalCode.
+#' \item CountryRegion.
+#' \item Wibpage. A fictitous URL
+#' \item Notes.
+#' }
+"Employees"
+
+#' Northwind Suppliers data table
+#'
+#' This data table is part of the freely downloadable Northwind sample database.
+#' They are useful datasets for demonstrating such things as SQL queries and
+#' business data analytics. Loading the Northwind dataset will place this and other
+#' related datanto your environment.
+#' @usage data(Northwind)
+#' @keywords data
+#' @format A dataframe with 9 rows and 15 variables:
+#' \itemize{
+#' \item ID.
+#' \item Company. name of the customer
+#' \item LastName.
+#' \item FirstName.
+#' \item EmailAddress
+#' \item JobTitle.
+#' }
+"Suppliers"
