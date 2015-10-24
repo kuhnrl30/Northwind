071850d91ff5db79f82e4bfdef38ea821bd29e83
Ryan <kuhnrl30@gmail.com>
2015-10-23 23:37:29 -0400

    Updated documentation

--- R/Northwind.R
+++ R/Northwind.R
@@ -80,3 +80,37 @@ NULL
 #' \item JobTitle.
 #' }
 "Suppliers"
+
+#' Northwind Inventory Transactions data table
+#'
+#' This data table is part of the freely downloadable Northwind sample database.
+#' They are useful datasets for demonstrating such things as SQL queries and
+#' business data analytics. Loading the Northwind dataset will place this and other
+#' related datanto your environment.
+#' @usage data(Northwind)
+#' @keywords data
+#' @format A dataframe with 102 rows and 6 variables:
+#' \itemize{
+#' \item TransactionID.
+#' \item TransactionType.
+#' \item TrnsactionCreatedDate.
+#' \item TransactionModifiedDate.
+#' \item ProductID.
+#' \item Quantity.
+#' }
+"InventoryTransactions"
+
+#' Northwind Inventory Transactions Type data table
+#'
+#' This data table is part of the freely downloadable Northwind sample database.
+#' They are useful datasets for demonstrating such things as SQL queries and
+#' business data analytics. Loading the Northwind dataset will place this and other
+#' related datanto your environment.
+#' @usage data(Northwind)
+#' @keywords data
+#' @format A dataframe with 4 rows and 2 variables:
+#' \itemize{
+#' \item ID.
+#' \item TypeName.
+#' }
+"InventoryTransactionTypes"
