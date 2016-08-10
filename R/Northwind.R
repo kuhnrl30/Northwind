d3aa656a92b205ebf28307e7192fc346e1a2b9a0
Ryan <kuhnrl30@gmail.com>
2016-08-09 22:01:01 -0400

    Update documentation

--- R/Northwind.R
+++ R/Northwind.R
@@ -2,7 +2,20 @@
 #'
 #'The Northwind package is a collection of database tables from the freely downloadable
 #'Northwind sample database  They are useful datasets for demonstrating such things as
-#'SQL queries and business data analytics.
+#'SQL queries and business data analytics. The tables in this package include:
+#'\itemize{
+#'\item Customers
+#'\item Employees
+#'\item InventoryTransactions
+#'\item InventoryTransactionTypes
+#'\item Invoices
+#'\item Orders
+#'\item PurchaseOrderDetails
+#'\item PurchaseOrders
+#'\item Shippers
+#'\item Suppliers
+#'\item 
+#'}
 #' @name Northwind
 #' @docType package
 NULL
